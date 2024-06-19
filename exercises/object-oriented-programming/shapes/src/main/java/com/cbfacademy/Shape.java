89a9a216a6c502604166aad5b95019d6d485d08b
Nyadej <naomiadejumo@gmail.com>
2024-06-19 22:08:16 +0100

    completed abstraction tasks

--- exercises/object-oriented-programming/shapes/src/main/java/com/cbfacademy/Shape.java
+++ exercises/object-oriented-programming/shapes/src/main/java/com/cbfacademy/Shape.java
@@ -3,6 +3,9 @@ package com.cbfacademy.shapes;
 public abstract class Shape {
   
   protected double radius;
+  protected double length;
+  protected double width;
+  protected double height;
 
   abstract double getArea();
 
@@ -10,4 +13,41 @@ public abstract class Shape {
     return this.getClass().getSimpleName();
   }
   
+  public class Sphere extends Shape {
 
+    public Sphere(double radius) { // constructor method
+      this.radius = radius;
+    }
+  
+    @Override
+    public double getArea() {
+      return (4 * Math.PI * (radius*radius));
+    }
+  }
+
+  public class Rectangle extends Shape {
+
+    public Rectangle(double length, double width) { // constructor method
+      this.length = length;
+      this. width = width;
+    }
+
+    @Override
+    public double getArea() {
+      return (length * width);
+    }
+  }
+
+  public class Cylinder extends Shape {
+
+    public Cylinder(double radius, double height) {
+      this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+      return (height * Math.PI * (radius*radius));
+    }
+  }
+
+  }
