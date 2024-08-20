cb20ea0cb0d4c8bb08435e353b9072b60ed8acc4
Nyadej <naomiadejumo@gmail.com>
2024-08-20 21:36:27 +0100

    Implement initial FizzBuzz logic for multiples of 3, and add basic tests for Fizz cases

---
+++ exercises/test-driven-development/src/main/java/com/cbfacademy/FizzBuzz.java
@@ -0,0 +1,22 @@
+package com.cbfacademy;
+
+public class FizzBuzz {
+    
+    public static String get(int number) {
+        if (number %3 == 0) {
+            return "Fizz";
+        }
+        return Integer.toString(number);
+    }
+}
+
+        /*if (number %15 == 0) {
+            return "FizzBuzz";
+        } else if (number %3 == 0) {
+            return "Fizz";
+        /* } else if (number %5 == 0) {
+            return "Buzz"; 
+        } else {
+            return Integer.toString(number);
+        }*/
+        
--- exercises/test-driven-development/src/test/java/com/cbfacademy/AppTest.java
+++ exercises/test-driven-development/src/test/java/com/cbfacademy/AppTest.java
@@ -1,5 +1,7 @@
 package com.cbfacademy;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,7 +16,37 @@ public class AppTest {
     @DisplayName("creates the app")
     public void createsAnApp() {
         final App app = new App();
+        assertThat(app, is(notNullValue()));
+
+    }
+
+    @Test
+    @DisplayName("return 'Fizz' when the input is divisible by 3")
+    public void printsFizz() {
+        assertEquals("1", FizzBuzz.get(1));
+        assertEquals("2", FizzBuzz.get(2));
+        assertEquals("Fizz", FizzBuzz.get(3));
+    }
+
+    /*@Test
+    @DisplayName("return 'Buzz' when the input is divisible by 5")
+    public void testReturnBuzz() {
+        final FizzBuzz fizzBuzz = new FizzBuzz();
+            
+            assertThat(fizzBuzz, is(15));
+        }
+
+    @Test
+    @DisplayName("return 'FizzBuzz' when the input is 15")
+    public void testReturnFizzBuzz() {
+        final FizzBuzz fizzBuzz = new FizzBuzz();
+
+            assertThat(fizzBuzz, is(15));
+        }
+
+    public void createsAnApp() {
+        final App app = new App();
 
         assertThat(app, is(notNullValue()));
-    }
+    }*/
 }
