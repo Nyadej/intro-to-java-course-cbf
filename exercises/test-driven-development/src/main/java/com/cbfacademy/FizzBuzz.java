db6d39497ba2ea360fd5f6b8e7940ed6985292c3
Nyadej <naomiadejumo@gmail.com>
2024-08-20 22:36:41 +0100

    Add 'FizzBuzz' logic for multiples of 15; updated tests and confirmed all tests passing

--- exercises/test-driven-development/src/main/java/com/cbfacademy/FizzBuzz.java
+++ exercises/test-driven-development/src/main/java/com/cbfacademy/FizzBuzz.java
@@ -3,23 +3,15 @@ package com.cbfacademy;
 public class FizzBuzz {
     
     public static String get(int number) {
-        if (number %3 == 0) {
-            return "Fizz";
-        } else if (number %5 == 0) {
-            return "Buzz"; 
-        } else {
-        return Integer.toString(number);
-        }
-    }
-}
-
-        /*if (number %15 == 0) {
+        if (number % 15 == 0) {
             return "FizzBuzz";
-        } else if (number %3 == 0) {
+        } else if (number % 3 == 0) {
             return "Fizz";
-        /* } else if (number %5 == 0) {
+         } else if (number % 5 == 0) {
             return "Buzz"; 
         } else {
             return Integer.toString(number);
-        }*/
+        }
+    }
+}
         
--- exercises/test-driven-development/src/test/java/com/cbfacademy/AppTest.java
+++ exercises/test-driven-development/src/test/java/com/cbfacademy/AppTest.java
@@ -2,6 +2,7 @@ package com.cbfacademy;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -21,18 +22,34 @@ public class AppTest {
     }
 
     @Test
+    @DisplayName("return 'FizzBuzz' when the input is divisible by 15")
+    public void fizzBuzzTest() {
+        assertEquals("FizzBuzz", FizzBuzz.get(15));
+    }
+
+    @Test
     @DisplayName("return 'Fizz' when the input is divisible by 3")
-    public void printsFizz() {
+    public void fizzTest() {
         assertEquals("1", FizzBuzz.get(1));
         assertEquals("2", FizzBuzz.get(2));
         assertEquals("Fizz", FizzBuzz.get(3));
         assertEquals("4", FizzBuzz.get(4));
+
     }
 
     @Test
     @DisplayName("return 'Buzz' when the input is divisible by 5")
-    public void printsBuzz() {
+    public void buzzTest() {
         assertEquals("Buzz", FizzBuzz.get(5));
+        assertEquals("Fizz", FizzBuzz.get(6));
+        assertEquals("7", FizzBuzz.get(7));
+        assertEquals("8", FizzBuzz.get(8));
+        assertEquals("Fizz", FizzBuzz.get(9));
+        assertEquals("Buzz", FizzBuzz.get(10));
+        assertEquals("11", FizzBuzz.get(11));
+        assertEquals("Fizz", FizzBuzz.get(12));
+        assertEquals("13", FizzBuzz.get(13));
+        assertEquals("14", FizzBuzz.get(14));
         }
 
 }
