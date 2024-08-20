b7f9be8490c87a325e13752fdeea0db25af44e13
Nyadej <naomiadejumo@gmail.com>
2024-08-20 22:47:53 +0100

    removed before all import, as not used.

--- exercises/test-driven-development/src/test/java/com/cbfacademy/AppTest.java
+++ exercises/test-driven-development/src/test/java/com/cbfacademy/AppTest.java
@@ -2,7 +2,6 @@ package com.cbfacademy;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
