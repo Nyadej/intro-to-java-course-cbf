5d9171238a96f05057a10e54c634cccad0bcaa6e
Nyadej <naomiadejumo@gmail.com>
2024-08-27 12:09:50 +0100

    updated production code - mvn successful

--- exercises/test-driven-development/src/main/java/com/cbfacademy/Leap.java
+++ exercises/test-driven-development/src/main/java/com/cbfacademy/Leap.java
@@ -5,6 +5,10 @@ public class Leap {
     public static boolean isLeap(int year) {
         if (year % 400 == 0) {
             return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else if (year % 4 == 0) {
+            return true;
         }
         return false;
     }
