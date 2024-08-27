9c851dc72224e23d03e040ef5d44317b059737cf
Nyadej <naomiadejumo@gmail.com>
2024-08-27 10:25:25 +0100

    created a bank class with a list to store accounts

--- exercises/object-oriented-programming/accounts/src/main/java/com/cbfacademy/accounts/Bank.java
+++ exercises/object-oriented-programming/accounts/src/main/java/com/cbfacademy/accounts/Bank.java
@@ -1,6 +1,12 @@
 package com.cbfacademy.accounts;
 
+import java.util.ArrayList;
+
 public class Bank extends Account {
-    
+    private List<Account> accounts;
+
+    public Bank() {
+        this.accounts = new ArrayList<>();
+    }
     
 }
